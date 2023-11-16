d60371d847662513e3be7ec10cb9d40fe680dc6f
Ryan Seeto <ryan.seeto@alum.utoronto.ca>
2023-11-16 11:24:03 -0500

    feat: add summarize_enrolment to summarize single time period

--- obri_consent_summary/assets.py
+++ obri_consent_summary/assets.py
@@ -1,1 +1,21 @@
+import numpy as np
+import pandas as pd
 
+def summarize_enrolment(start_date, end_date, redcap_df):
+    summary_dict = {}
+
+    redcap_df = redcap_df.replace("Consent not completed", np.NaN)
+    redcap_date = redcap_df.loc[
+        (pd.to_datetime(redcap_df['summary_consent_date']) >= str(start_date)) 
+        & (pd.to_datetime(redcap_df['summary_consent_date']) < str(end_date))
+    ]
+    
+    summary_dict['period'] = str(start_date) + ' - ' + str(end_date)
+    summary_dict['consent'] = sum(redcap_date['consent_obtained'] == 1)
+    
+    for rider_type in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
+        summary_dict['rider ' + rider_type] = sum(
+            redcap_date['summary_event_' + rider_type] == 1
+        )
+
+    return summary_dict
