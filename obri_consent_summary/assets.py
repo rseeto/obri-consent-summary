1ea602f6f54bbdd93234ebc71a8f1d03f2881936
Ryan Seeto <ryan.seeto@alum.utoronto.ca>
2023-11-17 12:06:44 -0500

    feat: add summarize_enrolment_total to summarize entire period

--- obri_consent_summary/assets.py
+++ obri_consent_summary/assets.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pandas as pd
+import datetime
+
+def get_redcap_record():
+    pass
 
 def summarize_enrolment(start_date, end_date, redcap_df):
     summary_dict = {}
@@ -19,3 +23,27 @@ def summarize_enrolment(start_date, end_date, redcap_df):
         )
 
     return summary_dict
+
+def summarize_enrolment_total():
+    
+    # initialize with the date we started consenting
+    start_date = datetime.date(2023, 9, 11)
+    today = datetime.date.today()
+    delta = datetime.timedelta(days=7)
+
+    redcap_df = get_redcap_record()
+
+    rows_list = []
+    while (start_date <= today):
+        rows_list.append(
+            summarize_enrolment(start_date, start_date+delta, redcap_df)
+        )
+        start_date += delta
+    df = pd.DataFrame(rows_list)
+    
+    # create row of totals
+    df.loc[len(df.index)] = df.sum(numeric_only=True)
+    df = df.astype({col: int for col in df.columns[1:]})
+    df.iloc[len(df.index)-1, 0] = 'Total'
+        
+    df.to_csv('summary.csv', index=False)
