a4669571b6faac6915409932f75ca007e69245dd
Ryan Seeto <ryan.seeto@alum.utoronto.ca>
2024-02-26 13:48:02 -0500

    feat: create dagster configurable resource for Google Cloud Platform

--- obri_consent_summary/resources/__init__.py
+++ obri_consent_summary/resources/__init__.py
@@ -4,6 +4,14 @@ import io
 from dagster import ConfigurableResource
 import dropbox
 
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.errors import HttpError
 
 class RedcapResource(ConfigurableResource):
     redcap_access_token: str
@@ -46,3 +54,50 @@ class DropboxResource(ConfigurableResource):
 
         with open(file_from, 'rb') as f:
             dbx.files_upload(f.read(), file_to,  mode=dropbox.files.WriteMode.overwrite)
+
+class GoogleResource(ConfigurableResource):
+    def get_credentials(self):
+        """
+        https://developers.google.com/drive/api/quickstart/python#configure_the_sample
+        """
+        SCOPES = 'https://www.googleapis.com/auth/drive.file'
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+
+    def upload_file(self, file_from, file_to):
+        """
+        """
+        SCOPES = 'https://www.googleapis.com/auth/drive.file'
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        try:
+            # create drive api client
+            service = build("drive", "v3", credentials=creds)
+
+            file_metadata = {"name": file_to}
+            media = MediaFileUpload(file_from, mimetype="text/csv")
+            file = (
+                service.files()
+                .create(body=file_metadata, media_body=media, fields="id")
+                .execute()
+            )
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+            file = None
+
+        return file.get("id")
