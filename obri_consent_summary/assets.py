f460c00f8ee96ea8f26540b5ee304331f6fc9da8
Ryan Seeto <ryan.seeto@alum.utoronto.ca>
2024-03-21 10:41:17 -0400

    docs: add docstrings to obri_consent_summary/assets.py

--- obri_consent_summary/assets.py
+++ obri_consent_summary/assets.py
@@ -6,6 +6,25 @@ from .resources import RedcapResource, DropboxResource
 
 @op
 def summarize_enrolment_date(start_date, end_date, redcap_df):
+    """Summarize enrolment based on given dates
+
+    Parameters
+    ----------
+    start_date : datetime.date
+        Start date of period of interest (inclusive)
+    end_date : datetime.date
+        End date of period of interest (exclusive)
+    redcap_df : pandas.DataFrame
+        Consent data from REDCap
+
+    Returns
+    -------
+    dict
+        Dictionary contains the keys:
+            period: time period of interest
+            consent: total number of people who consented
+            rider x: total number of people who consented to a specific rider
+    """
     summary_dict = {}
 
     redcap_df = redcap_df.replace("Consent not completed", np.NaN)
@@ -26,7 +45,28 @@ def summarize_enrolment_date(start_date, end_date, redcap_df):
 
 @op
 def summarize_enrolment_total(start_date, end_date, delta, redcap_df):
+    """Summarize enrolment based on given dates with delta resolution
 
+    Unlike summarize_enrolment_date, this function will group enrolment into delta periods. 
+    For example, if delta is set to 7, each row will represent the enrolment within a week.
+
+    Parameters
+    ----------
+    start_date : datetime.date
+        Start date of period of interest (inclusive)
+    end_date : datetime.date
+        End date of period of interest (exclusive)
+    delta : datetime.timedelta
+        Number of days represented in a DataFrame row
+    redcap_df : pandas.DataFrame
+        Consent data from REDCap
+
+    Returns
+    -------
+    pandas.DataFrame
+        Contains the period, consent, and all rider info over the period of interest,
+        with a delta resolution. The last row contains totals for the entire time period.
+    """
     rows_list = []
     while start_date <= end_date:
         rows_list.append(
@@ -44,6 +84,13 @@ def summarize_enrolment_total(start_date, end_date, delta, redcap_df):
 
 @asset
 def summarize_enrolment(redcap_api: RedcapResource):
+    """Create summary report and save locally
+
+    Parameters
+    ----------
+    redcap_api : RedcapResource
+        Dagster configurable resource to interact with REDCap
+    """
     # initialize with the date we started consenting
     start_date = datetime.date(2023, 9, 11)
     today = datetime.date.today()
@@ -57,6 +104,13 @@ def summarize_enrolment(redcap_api: RedcapResource):
 
 @asset(deps=[summarize_enrolment])
 def upload_summary(dropbox_api: DropboxResource):
+    """Upload the local report to cloud based storage
+
+    Parameters
+    ----------
+    dropbox_api : DropboxResource
+        Dagster configurable resource to interact with Dropbox
+    """
     file_from = 'data/OBRI Consent Summary.csv'
     file_to = '/OBRI Consent Summary/OBRI Consent Summary.csv'
 
